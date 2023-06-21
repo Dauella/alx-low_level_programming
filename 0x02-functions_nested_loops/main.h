747e62e89c6ebc7979cbb5fcffde382e79faaaa3
Dauella <sharonadongo3@gmail.com>
2023-06-21 01:38:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,26 +1,20 @@
 #ifndef Main_H
 #define Main_H
 
-/**
- * _putchar - Writes a character to standard output
- * @c: The character to be written
- *
- * Return: On success the character written. 
- */
-	int _putchar(char);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-	void print_times_table(int n);
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 
-	#endif
-
+#endif
