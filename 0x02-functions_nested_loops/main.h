23854ff7eee20b200301884480358c83a56ee890
Dauella <sharonadongo3@gmail.com>
2023-06-20 23:47:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef main_h
 #define main_h
+/*
+ * main header description
+ *
+ */
 
+int main(void)
 
 int _putchar(char);
 void print_alphabet(void);
@@ -10,7 +15,8 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);void times_table(void);
+void jack_bauer(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
